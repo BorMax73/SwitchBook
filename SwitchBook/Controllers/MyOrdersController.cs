0d59a4ad18af72a48bea3636fc0b8a8ba7da4cf5
Max Bortyanoy <maxbortyanoy@gmail.com>
2022-06-17 16:22:19 +0300

    r

--- SwitchBook/Controllers/MyOrdersController.cs
+++ SwitchBook/Controllers/MyOrdersController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SwitchBook.Data;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SwitchBook.Models;
 
 namespace SwitchBook.Controllers
 {
@@ -11,10 +14,16 @@ namespace SwitchBook.Controllers
         {
             _db = context;
         }
-        //public IActionResult Index()
-        //{
-            
-            
-        //}
+        public async Task<IActionResult> Index()
+        {
+            var myBooks= _db.Books.Where(x=>x.OwnerId == _db.Users.First(p=>p.UserName==User.Identity.Name).Id);
+            var orderRequest = await _db.Orders.Where(x=>myBooks.Select(b=>b.Id).Contains(x.FirstBookId) && x.IsConfirm ==false).ToListAsync();
+            var b1 = await _db.Books.Where(x=>orderRequest.Select(b=>b.FirstBookId).Contains(x.Id)).ToListAsync();
+            var b2 = await _db.Books.Where(x=>orderRequest.Select(b=>b.LastBookId).Contains(x.Id)).ToListAsync();
+
+            var books1 = _db.Books;  
+
+            return Ok(b1);
+        }
     }
 }
